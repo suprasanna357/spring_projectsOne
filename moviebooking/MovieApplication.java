598f0af7d01170cf956830a547c8c75a2297cc16
suprasanna357 <suprasanna357@gmail.com>
2026-08-23 18:44:35 +0530

    Add files via upload

---
+++ moviebooking/MovieApplication.java
@@ -0,0 +1,13 @@
+package moviebooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieApplication.class, args);
+	}
+
+}
